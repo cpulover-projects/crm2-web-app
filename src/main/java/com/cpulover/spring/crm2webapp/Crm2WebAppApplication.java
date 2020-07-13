7d73018d967cbdb4c8a0fbebfcf85b13ffd89baa
cpulover <justacpulover@gmail.com>
2020-07-13 07:37:02 +0200

    Setup Spring Boot

---
+++ src/main/java/com/cpulover/spring/crm2webapp/Crm2WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.cpulover.spring.crm2webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crm2WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crm2WebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpulover/spring/crm2webapp/Crm2WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpulover.spring.crm2webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crm2WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
